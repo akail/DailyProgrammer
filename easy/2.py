1d4e0471ccc911b74bdfbd2d16eb18c89569a1b9
Andrew Kail <andrew.a.kail@gmail.com>
2017-05-31 17:06:03 -0400

    Finish testing for 2 and 3

--- easy/2.py
+++ easy/2.py
@@ -1,28 +1,63 @@
 #!/usr/bin/env python3
 
-print("Input 2 of 3 variables F,M,A")
-F = input("F= ")
-M = input("M= ")
-A = input("A= ")
+import pytest
 
-if not A:
-    try:
-        F = float(F)
-        M = float(M)
-    except:
-        pass
-    print("A = {}".format(F/M))
-elif not F:
-    try:
-        A = float(A)
-        M = float(M)
-    except:
-        pass
-    print("F = {}".format(A*M))
-elif not M:
-    try:
-        A = float(A)
-        F = float(F)
-    except:
-        pass
-    print("M = {}".format(F/A))
+class InvalidInput(Exception):
+    pass
+
+def read_inputs():
+    print("Input 2 of 3 variables F,M,A")
+    F = input("F= ")
+    M = input("M= ")
+    A = input("A= ")
+    return F, M, A
+
+def get_result(F, M, A):
+    if not A:
+        try:
+            F = float(F)
+            M = float(M)
+        except ValueError:
+            raise InvalidInput('Invalid input.  Please use numeric values')
+        return "A", F/M
+    elif not F:
+        try:
+            A = float(A)
+            M = float(M)
+        except ValueError:
+            raise InvalidInput('Invalid input.  Please use numeric values')
+        return "F", A*M
+    elif not M:
+        try:
+            A = float(A)
+            F = float(F)
+        except ValueError:
+            raise InvalidInput('Invalid input.  Please use numeric values')
+        return "M", F/A
+    raise Exception("Invalid inputs")
+
+def main():
+    F,M,A = read_inputs()
+    print("{} = {}".format(*get_result(F,M,A)))
+
+if __name__ == '__main__':
+    main()
+
+
+def test_function():
+    assert get_result(1,2, None) == ('A', 0.5)
+    assert get_result(1,None, 2) == ('M', 0.5)
+    assert get_result(None,1, 2) == ('F', 2)
+
+def test_exceptions():
+    with pytest.raises(InvalidInput):
+        get_result('a',None,2)
+    with pytest.raises(InvalidInput):
+        get_result(None,'a',2)
+    with pytest.raises(InvalidInput):
+        get_result(2,None,'a')
+
+def test_all_inputs():
+    with pytest.raises(Exception):
+        get_result(1,2,3)
+
--- easy/3.py
+++ easy/3.py
@@ -14,7 +14,25 @@ def encrypt(text, shift=3):
 def decrypt(text, shift=3):
     return encrypt(text, -shift)
 
-test = encrypt('caesar')
-print('caesar')
-print(test)
-print(decrypt(test))
+def main():
+
+
+    test = encrypt('caesar')
+    print('caesar')
+    print(test)
+    print(decrypt(test))
+
+if __name__ == '__main__':
+    main()
+
+def test_encrypt():
+    assert encrypt('d') == 'a'
+    assert encrypt('a', shift=-3) == 'd'
+
+    assert encrypt('a') == 'x'
+    assert encrypt('x', shift=-3) == 'a'
+
+
+def test_ignored():
+    assert encrypt('1234567890!@#$%^&*()_+{}[]:";\'<>?,./') == '1234567890!@#$%^&*()_+{}[]:";\'<>?,./'
+    assert encrypt('d1234567890!@#$%^&*()_+{}[]:";\'<>?,./') == 'a1234567890!@#$%^&*()_+{}[]:";\'<>?,./'
